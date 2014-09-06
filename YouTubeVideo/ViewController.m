ddb410598f3c069e7e51b94578f8a9fcc4a9d673
Dylan Lewis <dylanslewis@me.com>
2014-09-06 15:55:42 +0100

    First version of the method

--- YouTubeVideo/ViewController.m
+++ YouTubeVideo/ViewController.m
@@ -16,7 +16,33 @@
             
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self showYouTubeVideo:@"Rr6w3tOeXpY" forX:0 forY:0 forWidth:200 forHeight:200];
+}
+
+- (void)showYouTubeVideo:(NSString *)url forX:(double)x forY:(double)y forWidth:(double)width forHeight:(double)height {
+    NSString *fullURL = [NSString stringWithFormat:@"http://www.youtube.com/embed/%@", url];
+    
+    UIWebView *videoView = [[UIWebView alloc] initWithFrame:CGRectMake(x, y, width, height)];
+    videoView.backgroundColor = [UIColor clearColor];
+    videoView.opaque = NO;
+    videoView.delegate = self;
+    [self.view addSubview:videoView];
+    
+    NSString *videoHTML = [NSString stringWithFormat:@"\
+                           <html>\
+                           <head>\
+                           <style type=\"text/css\">\
+                           iframe {position:absolute; top:50%%; margin-top:-130px;}\
+                           body {background-color:#000; margin:0;}\
+                           </style>\
+                           </head>\
+                           <body>\
+                           <iframe width=\"100%%\" height=\"240px\" src=\"%@\" frameborder=\"0\" allowfullscreen></iframe>\
+                           </body>\
+                           </html>", fullURL];
+    
+    [videoView loadHTMLString:videoHTML baseURL:nil];
 }
 
 - (void)didReceiveMemoryWarning {
